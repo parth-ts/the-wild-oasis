c8c0d19dca9009ccbbacabd177cff17403b8214e
Gowtham Reilly <gowtham@gowthamreilly.com>
2024-09-30 11:35:22 +0530

    add images

---
+++ apps/hotel/tests/auth.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from "@playwright/test";
+
+// 1. Test plan
+// 1. login page should be displayed when not logged in
+// 2. user should be able to login
+// 3. user should be able to logout
+// 4. when logged in, user should be able to see the dashboard
