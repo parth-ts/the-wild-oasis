65a25ea9bba2dbda55a478932c5e205493e614c9
Gowtham Reilly <gowtham@gowthamreilly.com>
2024-10-03 12:06:42 +0530

    small change

--- apps/guest/tests/cabin.spec.ts
+++ apps/guest/tests/cabin.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test("check if cabins page exist", async ({ page }) => {
   await page.goto("/cabins");
 
-  const pageTitle = page.getByText("Our Luxury Cabins");
+  const pageTitle = page.getByText("Our Luxury Cabin");
 
   await expect(pageTitle).toBeVisible();
 });
